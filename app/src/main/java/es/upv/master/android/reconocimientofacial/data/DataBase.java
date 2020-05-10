2fe51567d2886b70426a45c11859033f6e74ee4c
davidvalarezo <david.v.13@hotmail.es>
2020-05-10 05:57:43 +0200

    Parte de subir las etiquetas con sus respectivas coordenadas a la base de datos

--- app/src/main/java/es/upv/master/android/reconocimientofacial/data/DataBase.java
+++ app/src/main/java/es/upv/master/android/reconocimientofacial/data/DataBase.java
@@ -3,7 +3,9 @@ package es.upv.master.android.reconocimientofacial.data;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.util.Log;
@@ -11,11 +13,14 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
@@ -25,16 +30,20 @@ import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 import es.upv.master.android.reconocimientofacial.R;
 import es.upv.master.android.reconocimientofacial.model.Photo;
 
+import static android.content.Context.MODE_PRIVATE;
+
 public class DataBase {
    static public final String COLLECTION = "photosPrueba"; //"Mascarillas"
    static public String REFERENCE_FIRESTORAGE = "gs://mascarilla-440d4.appspot.com";
    public static StorageReference storageRef;
+   public static SharedPreferences preferencesLabels = null;
    public static boolean subiendoDatos = false;
 
    public static void registrarFoto(final long creation_date, final String id, String url) {
@@ -70,8 +79,7 @@ public class DataBase {
             @Override
             public void onFailure(@NonNull Exception exception) {
                subiendoDatos = false;
-               //mostrarDialogo(getApplicationContext(), "Ha ocurrido un error al" +
-               //    " subir la imagen o el usuario ha cancelado la subida.");
+
                Snackbar.make(activity.getCurrentFocus(), R.string.error_upload_photos, Snackbar.LENGTH_INDEFINITE)
                        .setAction("SI", new View.OnClickListener() {
                           @Override
@@ -97,10 +105,9 @@ public class DataBase {
                                firebaseUri.addOnSuccessListener(new OnSuccessListener<Uri>() {
                                   @Override
                                   public void onSuccess(Uri uri) {
-
+                                     //Recupero la URL donde se guardan las fotos
                                      String url = uri.toString();
                                      Log.e("Almacenamiento:", "the url is: " + url);
-                                     //String ref = imagenRef.getName();
                                      registrarFoto(System.currentTimeMillis(), idphotos.get(index), url);
                                      progresoSubida.dismiss();
                                      subiendoDatos = false;
@@ -160,24 +167,93 @@ public class DataBase {
       builder.create().show();
    }
 
-   public static void updateLabel(String id, String[] label, float[] x, float[] y) {
-      FirebaseFirestore db = FirebaseFirestore.getInstance();
-      DocumentReference photoRef = db.collection(COLLECTION).document(id);
-      int indexLabel = 0;
+   public static void updateLabel(String id, String label, float x, float y, int indexLabel) {
+      DocumentReference photoRef = getCollectionReferencePhotos().document(id);
       Map<String, Object> dataLabel = new HashMap<>();
-      for (int i=1; i<=label.length; i++){
-         dataLabel.put("label" + i, label);
-         dataLabel.put("x" + i, x);
-         dataLabel.put("y" + i, y);
-      }
-      for (int i=label.length; i<=9; i++){
-         dataLabel.put("label" + i, FieldValue.delete());
+         dataLabel.put("label" + indexLabel, label);
+         dataLabel.put("x" + indexLabel, x);
+         dataLabel.put("y" + indexLabel, y);
+         dataLabel.put("labelled", true);
+      photoRef.update(dataLabel);
+   }
+
+   public static void releaseLabel(String id, String label, float x, float y, int indexLabel) {
+      DocumentReference photoRef = getCollectionReferencePhotos().document(id);
+      Map<String, Object> dataLabel = new HashMap<>();
+         dataLabel.put("label" + indexLabel, FieldValue.delete());
          dataLabel.put("x" + indexLabel, FieldValue.delete());
          dataLabel.put("y" + indexLabel, FieldValue.delete());
-      }
-      dataLabel.put("labelled", true);
-      dataLabel.put("number_label", label.length); //TODO quitar si es sencillo
       photoRef.update(dataLabel);
+   }
+
+   public static void loadLabelsToPreference(final Context context, String id) {
+      DocumentReference PhotoRef = getCollectionReferencePhotos().document(id);
+      preferencesLabels = context.getSharedPreferences(
+              "es.upv.master.android.reconocimientofacial.labels", MODE_PRIVATE);
+      for (int i = 0; i < 9; i++) {
+         final int index = i + 1;
+         Task<DocumentSnapshot> query = PhotoRef.get()
+                 .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                       if (task.isSuccessful()) {
+                          String label = task.getResult().getString("label"+index);
+                          if(label != null){
+                             double x = task.getResult().getDouble("x" + index);
+                             double y = task.getResult().getDouble("y" + index);
+
+                             SharedPreferences.Editor editor = preferencesLabels.edit();
+                             editor.putFloat("x" + index, (float) x);
+                             editor.putFloat("y" + index, (float) y);
+                             editor.putString("label"+ index, label);
+                             editor.putInt("indexLabel"+ index, index);
+                             editor.commit();
+
+                          }
+                       }
+                    }
+                 });
+      }
+
+   }
+
+   public static ArrayList<Double> loadLabels(String id, final int index) {
+      DocumentReference PhotoRef = getCollectionReferencePhotos().document(id);
+      final Map<String, Object>  dataLabel = new HashMap<>();
+//      for (int i = 0; i < 9; i++) {
+ //        final int index = i + 1;
+      final ArrayList<Double> listPoint = new ArrayList<Double>();
+         Task<DocumentSnapshot> query = PhotoRef.get()
+                 .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                       if (task.isSuccessful()) {
+                          String label = task.getResult().getString("label"+index);
+                          if(label != null){
+                            // Map<String, Object> dataLabel = new HashMap<>();
+                             Double x = task.getResult().getDouble("x" + index);
+                             Double y = task.getResult().getDouble("y" + index);
+                             dataLabel.put("label"+ index, label);
+                             dataLabel.put("x" + index, task.getResult().getDouble("x" + index));
+                             dataLabel.put("y" + index, task.getResult().getDouble("y" + index));
+                             dataLabel.put("indexLabel", index);
+                             //listLabels.add(dataLabel);
+                             Log.d("ETIQUETAS", "x" + index + ": " + x + ",y" + index + ": " + y);
+                             System.out.println( "x" + index + ": " + x + ",y" + index + ": " + y);
+                             listPoint.add(x);
+                             listPoint.add(y);
+                          }
+                       }
+                    }
+                 });
+ //     }
+
+      System.out.println(dataLabel.get(0));
+      return listPoint;
+   }
+
+   public static CollectionReference getCollectionReferencePhotos(){
+      return FirebaseFirestore.getInstance().collection(COLLECTION);
    }
 
 }
--- app/src/main/java/es/upv/master/android/reconocimientofacial/ui/label/LabelActivity.java
+++ app/src/main/java/es/upv/master/android/reconocimientofacial/ui/label/LabelActivity.java
@@ -1,5 +1,6 @@
 package es.upv.master.android.reconocimientofacial.ui.label;
 
+import android.content.SharedPreferences;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.provider.DocumentsContract;
@@ -31,61 +32,62 @@ import java.util.Map;
 import java.util.jar.Attributes;
 
 import es.upv.master.android.reconocimientofacial.R;
-import es.upv.master.android.reconocimientofacial.data.Firebase;
+import es.upv.master.android.reconocimientofacial.data.DataBase;
 
-import static es.upv.master.android.reconocimientofacial.data.Firebase.COLLECTION;
+import static es.upv.master.android.reconocimientofacial.data.DataBase.getCollectionReferencePhotos;
+import static es.upv.master.android.reconocimientofacial.data.DataBase.preferencesLabels;
+
 
 public class LabelActivity extends AppCompatActivity implements View.OnTouchListener {
 
    String idPhoto;  //id de la colección a modificar
+   boolean islabelledPhoto;
    String nLabel = "1";
    ImageView photo;
-   TextView circle1;
-   TextView circle2;
-   TextView circle3;
-
    TextView[] circle = new TextView[9];
-   LinearLayout layoutCircles;
    ArrayList<String> listLabel;
+
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_label);
+      //Cargamos los parámetros
+      Bundle extras = getIntent().getExtras();
+      idPhoto = extras.getString("Id_photo");
+      islabelledPhoto = extras.getBoolean("Labelled_photo",false);
+      String uriPhoto = extras.getString("URL_photo");
+
       photo = findViewById(R.id.photo);
+      photo.setOnTouchListener(this);
+
       //Inicializo todos los círculos
       int circlesID[] = {R.id.txv_label1, R.id.txv_label2, R.id.txv_label3, R.id.txv_label4, R.id.txv_label5,
               R.id.txv_label6, R.id.txv_label7, R.id.txv_label8, R.id.txv_label9};
-
-      for(int i=0; i<circle.length; i++){
+      for (int i = 0; i < circle.length; i++) {
          circle[i] = findViewById(circlesID[i]);
       }
-      //Cargo todos las etiquetas
+      //TODO cambiar - Cargo todos las etiquetas
       listLabels();
 
-      photo.setOnTouchListener(this);
-      //Cargamos la imagen de storage
-      Bundle extras = getIntent().getExtras();
-      idPhoto = extras.getString("Id_photo");
-      boolean labelPhoto = extras.getBoolean("Labelled_photo");
-      String uriPhoto = extras.getString("URL_photo");
+      //Cargamos la imagen de URL
       Glide.with(getApplicationContext())
               .load(uriPhoto)
               .placeholder(R.drawable.mask_frontal)
               .into(photo);
-
    }
 
+   //TODO Esta función ya no es necesario, reemplazar por circle[Integer.valueOf(nLabel)]
    private TextView getCircle() {
       int index = Integer.valueOf(nLabel) - 1;
-      for(int i=0; i<circle.length; i++){
-         if(index == i) return circle[i];
+      for (int i = 0; i < circle.length; i++) {
+         if (index == i) return circle[i];
       }
       return null;
    }
 
    public void onButtonClic(View v) {
       nLabel = (String) v.getTag();
-      //Marcar botón como seleccionado, por ejemplo cam
+      //TODO Marcar botón como seleccionado, por ejemplo cam
       //Marcar resto de botones como NO seleccionado
       TextView circle = getCircle();
       if (circle.getVisibility() == View.VISIBLE) {
@@ -130,50 +132,8 @@ public class LabelActivity extends AppCompatActivity implements View.OnTouchList
    @Override
    protected void onResume() {
       super.onResume();
-
-      //Leer de la colección el id idPhoto
-      //Leemos los valores label1, x1, y1, x2, ... de la base de datos
-      //Inicializamos circle1, 2 y 3
-
-//     bucle n 1..3 hasta que no exista el campo
-//       String label = leer de el registro "label"+n
-//       float  x = leer de el registro "x"+n
-//       float  y = leer de el registro "y"+n
-//       nLabel = "no simetrico" -> "1" "mal ajustada" -> "2"
-//       TextView circle = getCircle()
-//       circle.setVisivility(VISIBLE)
-//       setCircle(circle, x, y)
-
-//      setCircle(circle1, -1, -1);
-//      setCircle(circle2, 0.9f, 0.9f); //NO FUNCIONA
-//      setCircle(circle3, -1, -1);
-       getLabels();
-   }
-
-   public void getLabels(){
-       FirebaseFirestore db = FirebaseFirestore.getInstance();
-       DocumentReference PhotoRef =  db.collection(COLLECTION).document(idPhoto);
-       for(int i=0; i < circle.length; i++){
-           final int index = i+1;
-           Task<DocumentSnapshot> query = PhotoRef.get()
-                   .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-               @Override
-               public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                   if (task.isSuccessful()) {
-                        Double x = task.getResult().getDouble("x"+index);
-                        Double y = task.getResult().getDouble("y"+index);
-                       Log.d("ETIQUETAS","x" + index + ": " + x + ",y" + index + ": " + y);
-                       if(x != null && y != null){
-                           double valorX =  x;
-                           double valorY = y;
-                           setCircle(circle[index-1],(float)valorX,(float)valorY);
-                       }
-                   }
-               }
-           });
-
-
-       }
+      if(islabelledPhoto)
+         loadLabels(idPhoto);
    }
 
    @Override
@@ -183,29 +143,72 @@ public class LabelActivity extends AppCompatActivity implements View.OnTouchList
       super.onPause();
    }
 
-   public void saveLabels(){
-      FirebaseFirestore db = FirebaseFirestore.getInstance();
-      DocumentReference PhotoRef =  db.collection(COLLECTION).document(idPhoto);
-      for(int i=0; i < circle.length; i++){
-         if(circle[i].getVisibility() == View.VISIBLE){
-            Map<String, Object> dataLabel = new HashMap<>();
-            int index = i+1;
-            dataLabel.put("name"+index, listLabel.get(i));
-            dataLabel.put("x"+index ,getX(circle[i]));
-            dataLabel.put("y"+index,getY(circle[i]));
-            dataLabel.put("number_label", index);
-            PhotoRef.update(dataLabel);
-            //PhotoRef.collection("LABEL").document("label"+index).set(dataLabel);
+   public void saveLabels() {
+      for (int i = 0; i < circle.length; i++) {
+         //Primero me aseguro que la foto ha sido etiquetada para eliminar las etiquetas
+         if(islabelledPhoto)
+         DataBase.releaseLabel(idPhoto, listLabel.get(i), getX(circle[i]), getY(circle[i]), i+1);
+         if (circle[i].getVisibility() == View.VISIBLE) {
+            DataBase.updateLabel(idPhoto, listLabel.get(i), getX(circle[i]), getY(circle[i]), i+1);
          }
       }
-      PhotoRef.update("labelled", true);
    }
 
-   public void listLabels(){
-      int[] labelResources = {R.string.label_1, R.string.label_2,  R.string.label_3, R.string.label_4, R.string.label_5,
+   //TODO tratar de pasar esta funcion a Firebase con parámetros adecuados
+   //No funciona porque loadLabelsToPreference es asíncrono
+   public void addLabelsToPhotos() {
+      DataBase.loadLabelsToPreference(getApplicationContext(), idPhoto);
+      for(int i = 1; i<=9; i++){
+         String label = preferencesLabels.getString("label"+i, null);
+         if(label != null){
+            double x = preferencesLabels.getFloat("x"+i, 0);
+            double y = preferencesLabels.getFloat("y"+i, 0);
+            setCircle(circle[i-1], (float)x, (float)y);
+         }
+      }
+
+   }
+
+   public void loadLabels(String id) {
+      DocumentReference PhotoRef = getCollectionReferencePhotos().document(id);
+      for (int i = 0; i < 9; i++) {
+      final int index = i + 1;
+      Task<DocumentSnapshot> query = PhotoRef.get()
+              .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                 @Override
+                 public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                       String label = task.getResult().getString("label"+index);
+                       if(label != null){
+                          double x = task.getResult().getDouble("x" + index);
+                          double y = task.getResult().getDouble("y" + index);
+                          setCircle(circle[index-1], (float)x, (float)y);
+                       }
+                    }
+                 }
+              });
+          }
+   }
+
+   //TODO OPCIONAL Podrías eliminar el método listLabels() si defines directamente un recurso array.string:
+/* https://stackoverflow.com/questions/23321449/how-to-list-android-string-resources-in-an-array
+   define your array in array.xml:
+   <?xml version="1.0" encoding="utf-8"?>
+<resources>
+    <string-array name="labels">
+        <item>barba</item>
+        <item>goma mal ...</item>
+    </string-array>
+ </resources>
+   then try this:
+   String[] listLabel = getResources().getStringArray(
+           R.array.dlabels);
+   */
+   public void listLabels() {
+      int[] labelResources = {R.string.label_1, R.string.label_2, R.string.label_3, R.string.label_4, R.string.label_5,
               R.string.label_6, R.string.label_7, R.string.label_8, R.string.label_9};
       listLabel = new ArrayList<String>();
-      for(int i=0; i < labelResources.length; i++){
+      for (int i = 0; i < labelResources.length; i++) {
          String label = getResources().getString(labelResources[i]);
          listLabel.add(i, label);
       }
