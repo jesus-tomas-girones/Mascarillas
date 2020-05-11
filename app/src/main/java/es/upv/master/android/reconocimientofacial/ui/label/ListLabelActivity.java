14c4cb0ecca98e3376919e215c49c0ebe724fe00
Jesus Tomas Girones <jtomas@upv.es>
2020-05-11 20:27:41 +0200

    - Arreglado botón pasar a siguiente foto - startListening() en onCreate y stopListening() en onDestroy

--- app/src/main/java/es/upv/master/android/reconocimientofacial/ui/label/ListLabelActivity.java
+++ app/src/main/java/es/upv/master/android/reconocimientofacial/ui/label/ListLabelActivity.java
@@ -1,15 +1,9 @@
 package es.upv.master.android.reconocimientofacial.ui.label;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -25,110 +19,116 @@ import es.upv.master.android.reconocimientofacial.model.Photo;
 
 
 public class ListLabelActivity extends AppCompatActivity {
-    private TabLayout tabs;
-    private RecyclerView recyclerView;
-    FirebaseFirestore db;
-    public static ListLabelAdapter adaptador;
-    public static final int REQUEST_CODE_NEXT_PHOTO = 1;
-    public static int position;
-    FirestoreRecyclerOptions<Photo> opciones;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_evaluator);
-        //
+   private TabLayout tabs;
+   private RecyclerView recyclerView;
+   FirebaseFirestore db;
+   public static ListLabelAdapter adaptador;
+   public static final int REQUEST_CODE_NEXT_PHOTO = 1;
+   public static int position;
+   FirestoreRecyclerOptions<Photo> opciones;
 
-        recyclerView = findViewById(R.id.recyclerview_photos);
-        db = FirebaseFirestore.getInstance();
-        listarFotos( false);
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_evaluator);
+      //
 
-        //
-        tabs = (TabLayout) findViewById(R.id.tabs);
-        String tab1 = getResources().getString(R.string.tab_lista_evaluados);
-        String tab0 = getResources().getString(R.string.tab_lista_sin_evaluar);
-        tabs.addTab(tabs.newTab().setText(tab0));
-        tabs.addTab(tabs.newTab().setText(tab1));
-        tabs.setTabMode(TabLayout.MODE_SCROLLABLE);
-        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                switch (tab.getPosition()) {
-                    case 0: //Sin Evaluar
-                        listarFotos( false);
-                        adaptador.startListening();
-                        break;
-                    case 1: //Evaluados
-                        listarFotos( true);
-                        adaptador.startListening();
-                        break;
-                }
-                //adaptador.notifyDataSetChanged();
+      recyclerView = findViewById(R.id.recyclerview_photos);
+      db = FirebaseFirestore.getInstance();
+      listarFotos(false);
+
+      //
+      tabs = (TabLayout) findViewById(R.id.tabs);
+      String tab1 = getResources().getString(R.string.tab_lista_evaluados);
+      String tab0 = getResources().getString(R.string.tab_lista_sin_evaluar);
+      tabs.addTab(tabs.newTab().setText(tab0));
+      tabs.addTab(tabs.newTab().setText(tab1));
+      tabs.setTabMode(TabLayout.MODE_SCROLLABLE);
+      tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+         @Override
+         public void onTabSelected(TabLayout.Tab tab) {
+            switch (tab.getPosition()) {
+               case 0: //Sin Evaluar
+                  listarFotos(false);
+                  adaptador.startListening();
+                  break;
+               case 1: //Evaluados
+                  listarFotos(true);
+                  adaptador.startListening();
+                  break;
             }
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {}
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {}
-        });
+            //adaptador.notifyDataSetChanged();
+         }
 
+         @Override
+         public void onTabUnselected(TabLayout.Tab tab) {
+         }
 
-    }
+         @Override
+         public void onTabReselected(TabLayout.Tab tab) {
+         }
+      });
+      //El adaptador escucha desde su creación y deja de hacerlo en su destrucción.
+      //De esta forma al entrar en LabelActivity no es destruido
+      adaptador.startListening();
+   }
 
-    @Override public void onStart() {
+/*    @Override public void onStart() { //
         super.onStart(); adaptador.startListening();
-    }
+    }*/
 
-    @Override public void onStop() {
-        super.onStop(); adaptador.stopListening();
-    }
+   @Override
+   public void onDestroy() { //Stop
+      //super.onStop();
+      super.onDestroy();
+      adaptador.stopListening();
+   }
 
-    public void listarFotos(boolean isEvaluated){
-        Query query = db.collection(DataBase.COLLECTION)
-                .orderBy("creation_date", Query.Direction.DESCENDING)
-                .whereEqualTo("labelled",isEvaluated);
-        opciones = new FirestoreRecyclerOptions
-                .Builder<Photo>()
-                .setQuery(query, Photo.class)
-                .build();
-        adaptador = new ListLabelAdapter(this, opciones);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adaptador);
+   public void listarFotos(boolean isEvaluated) {
+      Query query = db.collection(DataBase.COLLECTION)
+              .orderBy("creation_date", Query.Direction.DESCENDING)
+              .whereEqualTo("labelled", isEvaluated);
+      opciones = new FirestoreRecyclerOptions
+              .Builder<Photo>()
+              .setQuery(query, Photo.class)
+              .build();
+      adaptador = new ListLabelAdapter(this, opciones);
+      recyclerView.setLayoutManager(new LinearLayoutManager(this));
+      recyclerView.setAdapter(adaptador);
+      adaptador.setOnItemClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+            position = recyclerView.getChildAdapterPosition(view);
+            labelPhotoAtPossition(position);
+         }
+      });
+   }
 
-        adaptador.setOnItemClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                position = recyclerView.getChildAdapterPosition(view);
-                Photo photoItem = (Photo) adaptador.getItem(position);
-                String idPhoto = adaptador.getSnapshots().getSnapshot(position).getId();
-                Intent intent = new Intent(ListLabelActivity.this, LabelActivity.class);
-                //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.putExtra("Id_photo", idPhoto);
-                intent.putExtra("Labelled_photo", photoItem.isLabelled());
-                intent.putExtra("URL_photo", photoItem.getUrlPhoto());
-                //getApplicationContext().startActivity(intent);
-                startActivityForResult(intent, REQUEST_CODE_NEXT_PHOTO);
-            }
-        });
-    }
+   @Override
+   public void onActivityResult(int requestCode, int resultCode, Intent data) {
+      super.onActivityResult(requestCode, resultCode, data);
+      if (requestCode == REQUEST_CODE_NEXT_PHOTO && resultCode == RESULT_OK) {
+         //Si la actividad retorna indicando que queremos pasar a la siguiente foto
+         //incrementamos la posición y abrimos la nueva foto
+         position++;
+         if (position < adaptador.getItemCount()) {
+            labelPhotoAtPossition(position);
+         }
+      }
+   }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-            super.onActivityResult(requestCode, resultCode, data);
-            if (requestCode == REQUEST_CODE_NEXT_PHOTO  && resultCode  == RESULT_OK) {
-
-                Photo photoItem = (Photo) opciones.getSnapshots().get(position);
-                String idPhoto = adaptador.getSnapshots().getSnapshot(position).getId();
-                //String id = adaptador.getKey(position);
-                //Photo photoItem = (Photo) adaptador.getItem(position);
-                Context context = getApplicationContext();
-                Intent intent = new Intent(context, LabelActivity.class);
-                intent.putExtra("Id_photo", idPhoto);
-                intent.putExtra("Labelled_photo", photoItem.isLabelled());
-                intent.putExtra("URL_photo", photoItem.getUrlPhoto());
-                //context.startActivity(intent);
-                startActivityForResult(intent, REQUEST_CODE_NEXT_PHOTO);
-            }
-
-    }
-
+   void labelPhotoAtPossition(int possition) {
+       /**
+        * Abre LabelActivity con una foto para etiquetarla
+        * @param possition posición en el adaptador de la foto a etiquetar
+         */
+      Photo photoItem = (Photo) adaptador.getItem(position);
+      String idPhoto = adaptador.getSnapshots().getSnapshot(position).getId();
+      Intent intent = new Intent(ListLabelActivity.this, LabelActivity.class);
+      intent.putExtra("Id_photo", idPhoto);
+      intent.putExtra("Labelled_photo", photoItem.isLabelled());
+      intent.putExtra("URL_photo", photoItem.getUrlPhoto());
+      startActivityForResult(intent, REQUEST_CODE_NEXT_PHOTO);
+   }
 
 }
